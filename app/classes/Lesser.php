579344a0b864f859f5c8b5c9948d7bd8c6f1ea41
Ben Allfree <ben@benallfree.com>
2015-06-29 06:20:10 -0700

    5.2 compat

--- app/classes/Lesser.php
+++ app/classes/Lesser.php
@@ -5,13 +5,16 @@ class Lesser
   function __construct()
   {
     
-    add_action('init', function() {
-      if(!(isset($_GET['lessercss']) && $_GET['lessercss'])) return;
-      
-      $which = $_GET['lessercss'];
-      
-      die($which);
-    });
+    add_action('init', array($this, 'init'));
+  }
+  
+  function init()
+  {
+    if(!(isset($_GET['lessercss']) && $_GET['lessercss'])) return;
+    
+    $which = $_GET['lessercss'];
+    
+    die($which);
   }
   
   function render()
