9957e5a20842980426c596ba34b68c4cb300c3c7
Ben Allfree <ben@benallfree.com>
2015-07-10 06:14:41 -0700

    Checkpoint

--- app/classes/Lesser.php
+++ app/classes/Lesser.php
@@ -4,41 +4,25 @@ class Lesser
 {
   function __construct()
   {
-    add_action('wp_head', array($this, 'wp_head'));
+    add_action('wp_enqueue_scripts', array($this, 'wp_enqueue_styles'), 99);
     add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'plugin_action_links' );
-    add_action('admin_head', array($this, 'admin_head'));
+    add_action('admin_enqueue_scripts', array($this, 'admin_enqueue_scripts'));
   }
   function plugin_action_links( $links ) {
      $links[] = '<a href="'. esc_url( get_admin_url(null, 'admin.php?page=lesser_instruction_page') ) .'">Settings</a>';
      return $links;
   }
   
-  function stylesheet($url)
+  function wp_enqueue_styles()
   {
-    $url = htmlentities(apply_filters('lesser_css_url', $url));
-    return "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"$url\" />\n";
+    wp_enqueue_style('less-common', plugins_url('/lesser/app/render.php')."?mode=common");
+    wp_enqueue_style('less-frontend', plugins_url('/lesser/app/render.php')."?mode=frontend");
   }
   
-  function render_head($modes)
+  function admin_enqueue_scripts()
   {
-    $url = plugins_url("/lesser/app/render.php");
-    $data = get_option('Lesser_APF');
-    foreach($modes as $mode)
-    {
-      $key = "lesser_{$mode}_css";
-      if(!isset($data[$key])) continue;
-      echo($this->stylesheet($url."?mode={$mode}"));
-    }
-  }
-  
-  function wp_head()
-  {
-    $this->render_head(array('common', 'frontend'));
-  }
-  
-  function admin_head()
-  {
-    $this->render_head(array('common', 'admin'));
+    wp_enqueue_style('less-common', plugins_url('/lesser/app/render.php')."?mode=common");
+    wp_enqueue_style('less-admin', plugins_url('/lesser/app/render.php')."?mode=admin");
   }
 }
 new Lesser();
